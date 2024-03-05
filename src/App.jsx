3826fb50a8c14086ce4db1a2a54ad390bdd84a65
TomasArancibia <tomasarancibia@uc.cl>
2024-03-05 15:14:16 -0300

    Realese Traffic Light

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Light from './components/light';
 
 function App() {
-  const [selectedColor, setSelectedColor] = useState("")
   return (
     <>
       <div className="container-fluid">
